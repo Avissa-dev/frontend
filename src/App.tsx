a8a36e19ac6408f230c4a0e9d66d6c3e17444717
Bryan31542 <00063820@uca.edu.sv>
2024-06-08 14:55:39 -0600

    :lipstick: Feat: add label

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import { FaRegCircleDot, FaLocationDot } from 'react-icons/fa6'
 function App() {
   return (
     <div>
+      <h3 className=" font-montserrat font-bold text-lg tracking-widest">
+        ORIGEN
+      </h3>
       <Input text={'Desde'} Icon={FaRegCircleDot} />
+      <h3 className=" font-montserrat font-bold text-lg tracking-widest">
+        DESTINO
+      </h3>
       <Input text={'Hasta'} Icon={FaLocationDot} />
     </div>
   )
