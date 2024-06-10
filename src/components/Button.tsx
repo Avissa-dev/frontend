a7728a62c24036dd8381849b238bca574e142f94
Bryan31542 <00063820@uca.edu.sv>
2024-06-10 17:27:49 -0600

    :pencil2: Fix: text sizing

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export const Button = () => {
   return (
-    <button className="bg-purple-700 text-white font-bold text-xs py-2 px-4 w-fit rounded-lg">
+    <button className="bg-purple-700 text-white font-bold text-sm py-2 px-4 w-fit rounded-lg">
       Iniciar Ruta
     </button>
   )
